541354932913c02b9350e79d01f72c653793d251
Joe Schmeling <jinsd6@gmail.com>
2017-04-05 20:47:41 -0700

    Added user input

--- app.js
+++ app.js
@@ -3,6 +3,44 @@ console.log('starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
+var argv = require('yargs')
+	.command('create', 'Creates an account', function (yargs) {
+		yargs.options({
+			name: {
+				demand: true,
+				alias: 'n',
+				description: 'Your account name goes here',
+				type: 'string'
+			},
+			username: {
+				demand: true,
+				alias: 'u',
+				description: 'Your username goes here',
+				type: 'string'
+			},
+			password: {
+				demand: true,
+				alias: 'p',
+				description: "your password goes here",
+				type: 'string'
+			}
+		}).help('help');
+	})
+	.command('get', 'Retrieves account information', function (yargs) {
+		yargs.options({
+			name: {
+				demand: true,
+				alias: 'n',
+				description: 'Account password to retrieve',
+				type: 'string'
+			}
+		}).help('help');
+	})
+	.help('help')
+	.argv;
+
+var command = argv._[0]
+
 function createAccount(account) {
 	var accounts = storage.getItemSync('accounts');
 
@@ -11,22 +49,42 @@ function createAccount(account) {
 	}
 	
 	accounts.push(account);
+
 	storage.setItemSync('accounts', accounts);
 	return account;
 }
 
-function getAccount(accountName) {
+function getAccount() {
 	var accounts = storage.getItemSync('accounts');
 	var matchedAccount;
 
 	accounts.forEach(function (account) {
-		if(account.name === accountName){
+		if(account.name === argv.name){
 			matchedAccount = account;
 		}
 	});
 
 	return matchedAccount;
 }
+
+if(command === 'create') {
+	var createdAccount = createAccount({
+		name: argv.name,
+		username: argv.username,
+		password: argv.password
+	});
+	console.log('Account created');
+	console.log(createdAccount);
+} else if (command === 'get') {
+	var fetchedAccount = getAccount(argv.name);
+	if(fetchedAccount === 'undefined'){
+		console.log('Account not found');
+	} else {
+		console.log('Account found');
+		console.log(fetchedAccount);
+	}
+}
+
 /* Add items to storage
 storage.setItemSync('accounts', [{
  	username: 'Joe',
@@ -34,32 +92,11 @@ storage.setItemSync('accounts', [{
  }]);
 */
 
-//var accounts = storage.getItemSync('accounts');
-
-
-// var facebook = {
-// 	name: 'facebook',
-// 	username: 'jinsd6@gmail.com',
-// 	password: 'Password123!'
-// };
-
-// var united  = {
-// 	name: 'united',
-// 	username: 'WM418156',
-// 	password: 'Password123!'
-// };
-
-// console.log(createAccount(united));
-
 // var hawaiian = {
 // 	name: 'hawaiian',
 // 	username: 'schjo06',
 // 	password: 'Password123!'
 // };
-
-//console.log(createAccount(hawaiian));
-
-console.log(getAccount('united'));
 
 /******************
  accounts.push({
@@ -68,8 +105,17 @@ console.log(getAccount('united'));
  });
 ******************/
 
-// storage.setItemSync('accounts', accounts);
-// save using setItemSync
 
-// var accounts = storage.getItemSync('accounts');
-// console.log(accounts);
+
+
+
+
+
+
+
+
+
+
+
+
+
